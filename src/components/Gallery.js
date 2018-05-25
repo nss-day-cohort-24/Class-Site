5cdc1c788ba62f42174676ac7a8920606a7932be
Amber Sharpe <amberjsharpe@gmail.com>
2018-05-25 14:57:41 -0500

    importing images

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 // import { Link } from 'react-router';
 import './Home.css';
-import pink from './images/pink_swatch.jpg';
-import green from './images/green_swatch.jpg';
-import purple from './images/purple_swatch.jpg';
-import yellow from './images/yellow_swatch.jpg';
+// import pink from './images/pink_swatch.jpg';
+// import green from './images/green_swatch.jpg';
+// import purple from './images/purple_swatch.jpg';
+// import yellow from './images/yellow_swatch.jpg';
+import amber from '../class-pics/amber';
+import arthur from '../class-pics/arthur';
+import ben from '../class-pics/ben';
+import dan from '../class-pics/dan';
+import dylan from '../class-pics/dylan';
+import jeremy from '../class-pics/jeremy';
+import jesie from '../class-pics/jesie';
+import kelsey from '../class-pics/kelsey';
+import laura from '../class-pics/laura';
+import lindsay from '../class-pics/lindsay';
+import mark from '../class-pics/mark';
 
 
 class Gallery extends Component {
