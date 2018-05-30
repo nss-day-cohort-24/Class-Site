734f682d2507a1287d92d798b1d244f75361592f
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-05-30 16:49:04 -0500

    pull the h2 into home and meetus

--- src/components/MeetUs.js
+++ src/components/MeetUs.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import { Link } from 'react-router';
 import './Home.css';
 import './MeetUs.css';
+import '../App.css';
 import arrow from './images/down.png';
 import MeetUsMap from './MeetUs_Map';
 
@@ -9,8 +10,9 @@ function MeetUs() {
     return(
 
         <div className="meetUs">
-            <img src={arrow} className="arrow" alt="arrow animation"/>
-            <br />
+            {/* <img src={arrow} className="arrow" alt="arrow animation"/>
+            <br /> */}
+            <h2 className="pb-5">Nashville Software School Cohort 24</h2>
             <MeetUsMap />
             <MeetUsMap />
             <MeetUsMap />
