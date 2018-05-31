bbd43cad8ef6c4a2a01a463a7a0398c692f1b46c
Dylan B <giglett21@gmail.com>
2018-05-31 08:04:22 -0700

    wrapped index.js in browserROuter. added routes to nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import '.././App.css';
 import './Nav.css';
-
+import { Link } from 'react-router-dom'
 import C24Logo from  '.././images/nss24-logo.svg';
 import { Navbar } from 'reactstrap';
 
@@ -10,13 +10,13 @@ class Nav extends Component {
   render() {
     return (
       <Navbar className="d-flex justify-content-between navBar">
-       <img src={C24Logo} alt="C24 Class Logo" className="c24Logo"/>
+       <Link to='/'><img src={C24Logo} alt="C24 Class Logo" className="c24Logo"/></Link>
        <ul className="rightSideNav">
 
-          <li className="navList active">ABOUT </li>
-          <li className="navList">MEET </li>
-          <li className="navList">TECH </li>
-          <li className="navList">WORK </li>
+          <li className="navList active"><Link to='/About'> ABOUT </Link></li>
+          <li className="navList"><Link to='/Meet'>MEET</Link> </li>
+          <li className="navList"><Link to='/Tech'>TECH</Link> </li>
+          <li className="navList"><Link to='/Work'>WORK</Link> </li>
           </ul>
       </Navbar>
     );
