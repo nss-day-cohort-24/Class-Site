ecced58684b84c6193cafc11ebf67498a564a803
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-06-04 15:47:44 -0500

    add the id for about section

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export default class About extends React.Component {
         let textContent = this.props.text;
 
         return(
-          <section className="text-left justify-content-center section__about">
+          <section id="About" className="text-left justify-content-center section__about">
               <p className="col descAbout ">Nashville Software School's Cohort 24 Front-End Developer &#38; UX Designer apprentices have been in a full-time six-month immersive software development bootcamp focusing on Front-End development and UI/UX fundamentals. Apprentices experience daily hands on application of development fundamentals and principles through group and individual projects reflecting real world business problems.</p>
           </section>
 
