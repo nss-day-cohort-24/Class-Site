db76f3046698a6da0426e1388a9e1798ffa53ad4
Taylor Bailey <taylor.williams.bailey@gmail.com>
2018-06-05 16:00:35 -0500

    thank you css, gallery images look clickable

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -292,7 +292,7 @@ let classList = [
 
 let Gallery = () => {
     let gallery = classList.map((item, index) => 
-        <div key={index} >
+        <div className="imgDiv" key={index} >
             <Link to={{ pathname: `/Class-Site${item.link}`, state: { pic: item.pic, silly: item.silly, link: item.link, name: item.name, bio: item.bio, quote: item.quote, git: item.github_url, linkedIn: item.linkedin_url, email: item.email, site: item.site_url} }}><img className="img-thumbnail" alt="" src={item.pic}/></Link>
         </div>
     )
