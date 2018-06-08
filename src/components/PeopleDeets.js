75a219d4f7eacff11820922db795d14e5432077e
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-06-08 11:56:09 -0500

    fix mobile people page. messed up the full size though, so fix that

--- src/components/PeopleDeets.js
+++ src/components/PeopleDeets.js
@@ -33,23 +33,23 @@ class PeopleDeetz extends Component {
         let data = this.props.location.state;
         return (
             <div className="peopleDiv">
-                <div className="peopledeetzbackground d-flex justify-content-center">
-                <div className="picturediv">
-                <img src={this.state.imgSrc} className="classpicture m-5" alt="classpicture" onMouseEnter={this.funny} onMouseLeave={this.stopfunny} />
-                </div>
-                <div className="aboutdiv m-5 px-5 py-4 align-self-center text-left">
-                <h1 className="class-h1 black  text-center text-uppercase mb-3">{data.name}</h1>
-                <h3 className="class-h3 black  mb-3">"{data.quote}"</h3>
-                <p className="class-p black">
-                {data.bio}
-                </p>
-                </div>
-                <div className="socialdiv d-flex">
-                <a href={data.linkedIn} target="_blank" ><img src={linkedin} alt="" className="icon m-2" /></a>
-                <a href={data.git} target="_blank" ><img src={github} alt="" className="icon m-2" /></a>
-                <a href={`mailTo:${data.email}`} target="_blank" ><img src={email} alt="icon" className="icon m-2" /></a>
-                <a href={data.site} target="_blank" ><img src={personalsite} alt="icon" className="icon m-2" /></a>
-                </div>
+                <div className="d-flex justify-content-center peopledeetzpage">
+                    <div className="picturediv col-12 col-sm-5">
+                        <img src={this.state.imgSrc} className="classpicture" alt="classpicture" onMouseEnter={this.funny} onMouseLeave={this.stopfunny} />
+                    </div>
+                    <div className="aboutdiv col-12 col-sm-6 align-self-center text-left">
+                        <h1 className="class-h1 black  text-center text-uppercase mb-3">{data.name}</h1>
+                        <h3 className="class-h3 black  mb-3">"{data.quote}"</h3>
+                        <p className="class-p black">
+                            {data.bio}
+                        </p>
+                    </div>
+                    <div className="socialdiv col-12 col-sm-1 d-flex">
+                        <a href={data.linkedIn} target="_blank" ><img src={linkedin} alt="" className="icon m-2" /></a>
+                        <a href={data.git} target="_blank" ><img src={github} alt="" className="icon m-2" /></a>
+                        <a href={`mailTo:${data.email}`} target="_blank" ><img src={email} alt="icon" className="icon m-2" /></a>
+                        <a href={data.site} target="_blank" ><img src={personalsite} alt="icon" className="icon m-2" /></a>
+                    </div>
                 </div>
                 <Link id="backButton" aria-current="false" to='/Class-Site/#Meet'>BACK</Link>
                 </div>
