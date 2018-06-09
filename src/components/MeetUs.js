ca3e8b1e084b658cb4ef2ee01216703890a6e5de
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-06-08 19:58:57 -0500

    get rid of extra div wrapper

--- src/components/MeetUs.js
+++ src/components/MeetUs.js
@@ -6,13 +6,9 @@ import Gallery from './Gallery';
 
 function MeetUs() {
     return(
-<div className='meetUs pt-5' id="Meet">
-        <div className="meetUs">
-            {/* <img src={arrow} className="arrow" alt="arrow animation"/>
-            <br /> */}
+        <div className='meetUs pt-5' id="Meet">
             <h2 className="mb-4">Nashville Software School Cohort 24</h2>
             <Gallery />
-        </div>
         </div>
     )
 }
