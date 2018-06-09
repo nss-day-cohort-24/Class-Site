5f5c21f71f1043fdc5ba58f6060250ad23ce2d5f
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-06-08 19:55:34 -0500

    working on the responsive tech icons

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -24,25 +24,25 @@ class Technologies extends Component{
                 <div>Loading...</div>
         )} else if(techLoaded){
         let techData = techInfo.tech.map((data,index) => (
-                <div className="mx-3 my-3" id={index} style={{width: 5 + 'em', height: 5 + 'em'}}>
+                <div className="techIcons" id={index} >
                     <img className="img-fluid filter" src={data.image} alt={data.name} />
                 </div>
             ))
         let designData = techInfo.design.map((data,index) => (
-                <div className="mx-3 my-3" id={index} style={{width: 5 + 'em', height: 5 + 'em'}}>
+                <div className="techIcons" id={index}>
                     <img className="img-fluid filter" src={data.image} alt={data.name} />
                 </div>
             ))
             return(
 
-                <div id="Tech" className="tech pt-5">
+                <div id="Tech" className="tech">
                     <h2>Technologies Used</h2>
                     <h3>Development</h3>
-                    <div className=" justify-content-center row mb-5">
+                    <div className="justify-content-center row px-5 mb-5">
                         {techData}
                     </div>
                     <h3>Design</h3>
-                    <div className="justify-content-center row mb-5">
+                    <div className="justify-content-center row px-5 mb-5">
                         {designData}
                     </div>
                 </div>        
